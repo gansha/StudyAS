71ccb7ac39e9f6126fac18f73b3189ad8a930240
shaer8023 <595688162@qq.com>
2018-03-18 17:38:19 +0800

    Initial commit

---
+++ app/src/main/java/gans/com/studyas/gsonfm/GsonFMTest.java
@@ -0,0 +1,42 @@
+package gans.com.studyas.gsonfm;
+
+/**
+ * Created by IBM on 2018/3/18.
+ */
+
+public class GsonFMTest {
+
+    /**
+     * name : tt
+     * pass : 23
+     * sex : 男
+     */
+
+    private String name;
+    private int pass;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int pass6) {
+        this.pass = pass;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
